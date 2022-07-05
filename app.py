7841f28f36c089cac2c84a4d35f13bd453b8fcf7
Jacqueline Wang'ombe <jacquelinewangombe@gmail.com>
2022-07-05 09:57:43 +0300

    Adding multiple images

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Contents of ~/my_app/streamlit_app.py
 import streamlit as st
 
+#[theme]
+font="serif"
+
 #Page Title
 st.set_page_config(page_title="AGROSMART AI", page_icon='tomato')
 
@@ -27,7 +30,7 @@ def page2():
     col1, col2 = st.columns([3,2])
     with col1:
         st.title ("Plant Disease Identifier App")
-        image = st.file_uploader("Upload your photo", type=['jpg', 'png'])
+        image = st.file_uploader("Upload your photo", type=['jpg', 'png'], accept_multiple_files=True)
         picture = st.camera_input("Take a picture")
         #if picture:
             #st.image(picture)
@@ -72,3 +75,13 @@ page_names_to_funcs = {
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
+with st.sidebar:
+    page_bg_img ="""
+<style>
+      .stApp {
+  background-image: url("https://images.unsplash.com/photo-1612670940073-8aed2145ebc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTZ8fHRvbWF0byUyMHdoaXRlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1296&q=60");
+  background-size: cover;
+}
+</style>
+"""
+    st.markdown(page_bg_img, unsafe_allow_html=True)
